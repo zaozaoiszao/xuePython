ab6db9075cd116e369f65f154320f3809f02edf8
zaozaoiszao <3154254646@qq.com>
2024-08-25 12:14:23 +0800

    更正了temp

--- temp.py
+++ temp.py
@@ -3,8 +3,4 @@ while True:
         #open
         data = file.read()
         data = data.replace(" ", "")
-<<<<<<< HEAD
-        
-=======
-        
->>>>>>> c1bd6261c9be880decfea9d603346e5d3c79c9f8
+
