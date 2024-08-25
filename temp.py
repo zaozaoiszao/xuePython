0d8221ddcedc9bbcccfffd66ea8bef71c2ad5b4d
zaozaoiszao <3154254646@qq.com>
2024-08-25 13:08:52 +0800

    ten all is OK

--- temp.py
+++ temp.py
@@ -1,10 +1,1 @@
-while True:
-    with open("temp.txt", "r") as file:
-        #open
-        data = file.read()
-        data = data.replace(" ", "")
-<<<<<<< HEAD
-        
-=======
-        
->>>>>>> c1bd6261c9be880decfea9d603346e5d3c79c9f8
+input()
