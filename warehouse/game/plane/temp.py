6103380af3e07e97eb5a373f7a46925ce393fa44
zaozaoiszao <3154254646@qq.com>
2024-08-30 14:52:39 +0800

    ```调整游戏窗口尺寸以适配不同屏幕
    调整了`warehouse/game/plane/temp.py`中的窗口大小设置，将宽度从480增加到1410，高度从700增加到711，以更好地适配不同显示屏幕。同时更新了`pygame.display.set_mode()`以反映这些更改，确保游戏界面在不同分辨率的显示器上能正确显示。
    ```

--- warehouse/game/plane/temp.py
+++ warehouse/game/plane/temp.py
@@ -7,8 +7,8 @@ import random
 pygame.init()
 
 # 设置窗口大小
-screen_width = 480
-screen_height = 700
+screen_width = 1410
+screen_height = 711
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("zaozao")
 
